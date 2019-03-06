43d1c0e7a8c0a75a43b7a893cc848847e966c4bb
carly <carly@noukod.com>
2019-03-06 21:01:55 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -6,10 +6,10 @@ function init() {
     let index = 0;
     document.body.addEventListener('keydown', function(e) {
       const key = parseInt(e.detail || e.which);
-  
+
       if (key === code[index]) {
         index++;
-  
+
       if (index === code.length) {
         alert('Hurray!');
       }
@@ -18,6 +18,6 @@ function init() {
     }
   });
   }
-  
+
   init();
 }
